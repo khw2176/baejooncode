85e9b0618480db3c82a73df054a9b27bc57dc626
khw2176 <khw2176@gmail.com>
2025-12-26 07:26:51 +0900

    [level 0] Title: 짝수의 합, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int solution(int n) 
+    {
+        int answer = 0;
+        
+        for(int i = 0; i <= n; i++)
+        {            
+            if(i % 2 == 1)
+            {
+                continue;
+            }
+            else if(i % 2 ==0)
+            {
+                answer += i;
+            }           
+        }
+        return answer;
+    }
+}
