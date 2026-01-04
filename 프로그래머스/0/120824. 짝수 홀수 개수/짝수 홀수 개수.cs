8f09780b4b16462e9ee7ecb1a514fdc4d059802b
khw2176 <khw2176@gmail.com>
2026-01-05 08:29:43 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list) 
+    {
+        int a = 0;
+        int b = 0;
+        
+        for(int i = 0; i < num_list.Length; i++)
+        {
+            if(num_list[i] % 2 == 0)
+            {
+                a++;
+            }
+            else
+            {
+                b++;
+            }
+        }
+        
+        return new int[] {a, b};
+    }
+}
