36ac05a73851ebfd01b52f30decfb9a0a4bf9c00
khw2176 <khw2176@gmail.com>
2026-01-05 08:17:02 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+
+public class Solution 
+{
+    public string solution(string my_string, int n) 
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        foreach(char c in my_string)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                sb.Append(c);
+            }
+        }
+        
+        return sb.ToString();
+    }
+}
